477b14f5a0ca97ec9bf0d94bca9ed6dec95d3d1b
Erik Toor <eriktoor@gmail.com>
2020-04-14 21:31:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from videoDetails import getVideoViews
 from updateVideoTitle import changeVideoTitle
+from videoDetailsRequest import getViews
 import time
  
 
@@ -9,24 +9,36 @@ class creds:
         self.credentials = False 
         self.flow = False 
         self.youtube = False 
+        self.viewCount = 0 
         self.num = 0
-        
-def main():
-    c = creds() 
-    timeout = 12000
-    while (c.num < 5): 
-        id = [YOUR-VIDEO-ID]
-        viewCount = getVideoViews(id, c)
-        print(viewCount)
 
-        changeVideoTitle(viewCount, id, c)
-        print("Go to https://www.youtube.com/watch?v=" + str(id) + " and check out the name.")
+        
+def main(): 
+    c = creds()
+    timeout = 720 
+    while(c.num < 20,000): 
+        id = "7-74A-FKWxo"
+        API_KEY = "YOUR-API-KEY"
+        viewCount = int(getViews(id, API_KEY))
+        if viewCount != c.viewCount: 
+            changeVideoTitle(viewCount, id, c)
+            print("Changing viewCount...")
+            print("Go to https://www.youtube.com/watch?v=" + str(id) + " and check out the name.")
+            timeout = 720
+        else: 
+            print("The view count is the same as it was so we are not changing")
+            timeout = 240 
+
+        c.viewCount = viewCount 
 
         for i in range(timeout):
             time.sleep(1)
-            print(timeout-i)
+            if (timeout - i) % 60 == 0: 
+                print(timeout-i)
+        print("Completed running time number " + str(c.num) + ".")
         c.num += 1 
-        timeout += 10
+
 
 if __name__ == "__main__": 
     main()
+
